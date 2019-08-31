03c85d7e9b536388f4bd17f8536756a6a87d496f
anischenkozhenya <43790223+anischenkozhenya@users.noreply.github.com>
2019-08-31 14:06:42 +0300

    +task2

---
+++ SynchronizationConstructs/Task2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+//Преобразуйте пример событийной блокировки таким образом, 
+//чтобы вместо ручной обработки использовалась автоматическая.
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var auto = new AutoResetEvent(false);
+            var thread = new Work("1", auto);
+            Console.WriteLine("Основной поток ожидает событие.\n");
+            auto.WaitOne();
+            Console.WriteLine("\nОсновной поток получил уведомление о событии от первого потока.\n");
+
+            thread = new Work("2", auto);
+            Console.WriteLine("Основной поток ожидает событие.\n");
+            auto.WaitOne();
+
+            Console.WriteLine("\nОсновной поток получил уведомление о событии от второго потока.");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SynchronizationConstructs/Task2/Work.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Task2
+{
+    class Work
+    {        
+        readonly Thread thread;
+        readonly AutoResetEvent auto;
+        public Work(string name, AutoResetEvent auto)
+
+        {
+            this.thread = new Thread(this.Run) { Name = name };
+            this.auto = auto;
+            this.thread.Start();
+        }
+
+        void Run()
+        {
+            Console.WriteLine("Запущен поток " + thread.Name);
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(". ");
+                Thread.Sleep(200);
+            }
+
+            Console.WriteLine("\nПоток " + thread.Name + " завершен");
+            auto.Set();
+        }
+    }
+}
